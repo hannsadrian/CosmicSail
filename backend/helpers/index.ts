cc8aa5ca13dd0f753321a70d918dd03951a7b44e
Adrian <hannsadrian@gmail.com>
2020-06-11 08:03:10 +0200

    Add function to search users from db

--- backend/helpers/index.ts
+++ backend/helpers/index.ts
@@ -1,4 +1,24 @@
+import {ClientType} from "../enums";
 const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+  username: String,
+  password: String,
+  type: Number,
+  isAdmin: Boolean
+});
+
+class UserEntity {
+  username: string
+  type: ClientType
+  isAdmin: boolean
+
+  constructor(username: string, type: ClientType, isAdmin: boolean) {
+    this.username = username
+    this.type = type
+    this.isAdmin = isAdmin
+  }
+}
 
 export class DBHelper {
   public connected: boolean = false;
@@ -29,6 +49,32 @@ export class DBHelper {
 
   public saveSample(): void {
     const Cat = mongoose.model('Cat', { name: String });
+  public async findUser(username:string): Promise<UserEntity> {
+    return new Promise((resolve, reject) => {
+      User.findOne({username: username}).exec((err, user) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (user == null) {
+          resolve(null);
+          return;
+        }
+
+        let type: ClientType;
+        switch (user.type) {
+          case 0:
+            type = ClientType.BOAT;
+            break;
+          case 1:
+            type = ClientType.CONTROLLER;
+            break;
+        }
+        resolve(new UserEntity(user.username, type, user.isAdmin || false));
+      })
+    })
+  }
+
 
     const kitty = new Cat({ name: 'Zildjian' });
     kitty.save().then(() => console.log('meow'));
