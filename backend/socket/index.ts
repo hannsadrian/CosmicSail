5e350b3292000280cf01bb9f01ab85ba4f543055
Adrian <hannsadrian@gmail.com>
2020-06-17 01:42:26 +0200

    Update socket

--- backend/socket/index.ts
+++ backend/socket/index.ts
@@ -1,9 +1,9 @@
-import {onlyVerifyJWT} from "../routes/auth"
+import {verifyJWT} from "../helpers/AuthUtils";
 import {getDBHelper} from "../"
 import {DBHelper} from "../helpers"
 const io = require('socket.io')();
 
-class Boat {
+class Entity {
   socket: any
   data: any
 
@@ -26,23 +26,37 @@ export class SocketHandler {
     io.on('connection', async client => {
       let object: object;
       try {
-        object = onlyVerifyJWT(client.handshake.query["token"])
+        object = verifyJWT(client.handshake.query["token"])
+        // @ts-ignore
         if (object.type === "boat") {
-          this.boats.push(new Boat(client, await this.helper.getBoat(object.id)))
+          // @ts-ignore
+          this.boats.push(new Entity(client, await this.helper.getBoat(object.id)))
+        } else {
+          // @ts-ignore
+          this.controllers.push(new Entity(client, await  this.helper.findUser(object.username)))
         }
       } catch(err) {
         client.disconnect();
         return;
       }
 
+      // TODO: Register actual events for data transmission
+
       client.on("disconnect", () => {
+        // @ts-ignore
         if (object.type === "boat")
-          this.boats.forEach((boat: Boat, index: number) => {
+          this.boats.forEach((boat: Entity, index: number) => {
+            // @ts-ignore
             if (boat.data.id == object.id)
               this.boats.splice(index,1)
           })
+        else
+          this.controllers.forEach((controller: Entity, index: number) => {
+            // @ts-ignore
+            if (controller.data.username == object.username)
+              this.controllers.splice(index,1)
+          })
       })
-
     });
   }
 
