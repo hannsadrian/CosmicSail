d6c60eb0316f36ceb3f5ab19d6402b524f0293a5
Adrian <hannsadrian@gmail.com>
2020-06-21 15:13:47 +0200

    Polish

--- frontend/src/views/Boats.svelte
+++ frontend/src/views/Boats.svelte
@@ -2,41 +2,44 @@
     import {onMount} from "svelte";
     import {Link} from "svelte-routing";
     import axios from "axios";
-    import SegmentedControl from "../components/SegmentedControl.svelte";
+    import InlineError from "../components/InlineError.svelte";
+    import Loading from "../components/Loading.svelte";
 
-    let boats = [];
-    let loading = true;
+    let boats = getBoats();
 
-    onMount(() => {
-        axios.get(process.env.APIURL + "/user/boats?username=" + localStorage.getItem("username") + "&jwt=" + localStorage.getItem("token")).then(res => {
-            boats = res.data;
-            loading = false;
-        }).catch(err => {
-            console.log(err)
-        })
-    })
+    async function getBoats() {
+        return await axios.get(process.env.APIURL + "/user/boats?username=" + localStorage.getItem("username") + "&jwt=" + localStorage.getItem("token"))
+    }
 </script>
 
 <div class="w-full pt-20 pb-10 dark:text-white px-5 sm:px-16">
+    <button class="text-sm font-semibold uppercase text-gray-600 tracking-wider" on:click={() => boats = getBoats()}>
+        Reload
+    </button>
+
     <h1 class="font-dosis text-4xl font-bold">Boats</h1>
     <p>Control your boats from here.</p>
 
-    {#if loading}
-        <p class="mt-4 italic">Loading...</p>
-    {/if}
-
-    <hr class="opacity-0 my-4">
-
-    {#each boats as boat, index}
-        <Link to={"/boats/"+boat.id}>
-            <div class="my-3 rounded-lg shadow-sm hover:shadow-lg transition duration-200 flex bg-white dark:bg-gray-900 p-4 w-full">
-                <div class="my-auto bg-red-600 rounded-full h-4 w-4 ml-2 mr-4"></div>
-                <div>
-                    <p class="text-sm font-medium text-gray-500">{boat.model} • {boat.id}
-                    </p>
-                    <h1 class="text-xl font-bold">{boat.name}</h1>
+    {#await boats}
+        <Loading/>
+    {:then result}
+        <hr class="opacity-0 my-4">
+        {#each result.data as boat, index}
+            <Link to={"/boats/"+boat.id}>
+                <div class="my-3 rounded-lg shadow-sm hover:shadow-lg transition duration-200 flex bg-white dark:bg-gray-900 p-4 w-full">
+                    <div class="my-auto bg-red-600 rounded-full h-4 w-4 ml-2 mr-4"></div>
+                    <div>
+                        <p class="text-sm font-medium text-gray-500">{boat.model} • {boat.id}</p>
+                        <h1 class="text-xl font-bold">{boat.name}</h1>
+                    </div>
                 </div>
-            </div>
-        </Link>
-    {/each}
+            </Link>
+        {/each}
+    {:catch err}
+        {#if err.response}
+            <InlineError>{err.response.data.message}</InlineError>
+        {:else}
+            <InlineError>Server connection failed</InlineError>
+        {/if}
+    {/await}
 </div>
