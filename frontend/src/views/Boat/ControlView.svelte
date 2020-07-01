5c6284e9bb08946c255007484578a9aa767dbbf1
Adrian <hannsadrian@gmail.com>
2020-07-01 21:09:11 +0200

    Parse gps metadata

--- frontend/src/views/Boat/ControlView.svelte
+++ frontend/src/views/Boat/ControlView.svelte
@@ -2,12 +2,25 @@
     import Map from "./ControlView/Map.svelte";
     export let socket;
 
-    let lat = 50.9;
-    let lng = 13.6;
+    let lat = 0;
+    let lng = 0;
     let rotation = 0;
+    let sats = 0;
+    let mode = 0;
+    let speed = 0;
+    let precision = [0, 0]
 
     socket.on("meta", data => {
         console.log(data)
+        sats = data.gps.sats;
+        mode = data.gps.mode;
+        if (data.gps.position != null) {
+            lat = data.gps.position[0]
+            lng = data.gps.position[1]
+            rotation = data.gps.heading
+            speed = data.gps.speed
+            precision = data.gps.precision
+        }
     })
 
     let rudder = 0;
@@ -16,8 +29,9 @@
     }
 </script>
 
-<div id="mapbox" class="sm:w-2/5 mt-4 mb-4 sm:mb-0">
+<div id="mapbox" class="sm:w-2/5 mt-4 mb-8 sm:mb-0">
     <Map {lng} {lat} {rotation} />
+    <p>M {mode} {"<->"} {sats} Sats {"<->"} {parseFloat(speed).toFixed(2)} m/s | {parseFloat(speed * 3.6).toFixed(2)} km/h</p>
 </div>
 <input type="range" min="-1" max="1" step="0.05" bind:value={rudder}>
 
