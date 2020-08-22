7aa71b930360f5bb3f62ac03f588bac948eb07a5
Hanns Adrian Böhme <hannsadrian@gmail.com>
2020-08-22 23:31:05 +0200

    Implement basic backend with gorm and fiber

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,14 @@ module CosmicSailBackend
 go 1.14
 
 require (
-	github.com/gofiber/cors v0.2.2 // indirect
-	github.com/gofiber/fiber v1.14.2 // indirect
+	github.com/gofiber/cors v0.2.2
+	github.com/gofiber/fiber v1.14.2
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.10.11 // indirect
+	github.com/maxwellhealth/go-dotaccess v0.0.0-20190924013105-74ea4f4ca4eb // indirect
+	github.com/oleiade/reflections v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.16.0 // indirect
 	golang.org/x/sys v0.0.0-20200821140526-fda516888d29 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
--- backend/server.go
+++ backend/server.go
@@ -1,18 +1,66 @@
 package main
 
 import (
-  "github.com/gofiber/fiber"
-  "github.com/gofiber/cors"
+	"github.com/gofiber/cors"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"time"
 )
 
+type Boat struct {
+	gorm.Model
+	BoatId     string `gorm:"type:varchar(5);unique_index"`
+	Token      string
+	Name       string
+	Series     string
+	Make       string
+	Online     bool
+	LastOnline time.Time
+}
+
 func main() {
-  app := fiber.New()
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("! WARNING ! -> Error while loading .env file")
+	} else {
+		log.Println("-> Loaded env file")
+	}
 
-  app.Use(cors.New())
+	// Connect to postgres
+	db, err := gorm.Open("postgres",
+		"host="+os.Getenv("POSTGRES_HOST")+
+		" port="+os.Getenv("POSTGRES_PORT")+
+		" user="+os.Getenv("POSTGRES_USER")+
+		" dbname="+os.Getenv("POSTGRES_DB")+
+		" password="+os.Getenv("POSTGRES_PSWD"))
+	if err != nil {
+		panic(err)
+	} else {
+		log.Println("-> Connected to database at " + os.Getenv("POSTGRES_HOST"))
+	}
+	defer db.Close()
+	db.AutoMigrate(&Boat{})
 
-  app.Get("/", func(c *fiber.Ctx) {
-    c.Send("Welcome!")
-  })
+	// Init webserver
+	app := fiber.New()
+	app.Use(cors.New())
 
-  app.Listen(3000)
+	// Register routes
+	app.Get("/v1/boats", func(c *fiber.Ctx) {
+		// TODO: Authenticate and only show boats for user
+		boats := []Boat{}
+		db.Find(&boats)
+		c.JSON(boats)
+	})
+
+	// Deliver static files
+	app.Static("/", "static")
+
+	// Start the server
+	panic(app.Listen(os.Getenv("SERVER_PORT")))
 }
