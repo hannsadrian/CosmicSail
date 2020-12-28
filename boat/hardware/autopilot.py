ee6c2ebd42c847f3d9059f757ddec581cc14ea8f
Adrian <hannsadrian@gmail.com>
2020-12-28 23:25:30 +0100

    Use proper math functions for rad and deg conversion

--- boat/hardware/autopilot.py
+++ boat/hardware/autopilot.py
@@ -62,17 +62,18 @@ def get_turning_angle(heading, bearing):
 def get_dest_bearing(lat1, lng1, lat2, lng2):
     y = math.sin(lng2 - lng1) * math.cos(lat2)
     x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(lng2 - lng1)
-    return math.atan2(y, x) * 180/math.pi
+    return math.degrees(math.atan2(y, x))
 
 
 def get_distance(lat1, lng1, lat2, lng2):
     R = 6371e3
-    phi1 = lat1 * math.pi / 180
-    phi2 = lat2 * math.pi / 180
-    delta_phi = (lat2 - lat1) * math.pi / 180
-    delta_lambda = (lng2 - lng1) * math.pi / 180
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lng2 - lng1)
 
     a = math.sin(delta_phi / 2) * math.sin(delta_phi / 2) + math.cos(phi1) * math.cos(phi2) * math.sin(
         delta_lambda / 2) * math.sin(delta_lambda / 2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     return R * c
+
