e41053aabaa6335307e394ab2d97e6fe9efcb379
Adrian <hannsadrian@gmail.com>
2020-12-30 01:27:14 +0100

    Start boat overview

--- frontend/src/contents/Boats.jsx
+++ frontend/src/contents/Boats.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Boats() {
+    const logout = () => {
+        localStorage.removeItem("username")
+        localStorage.removeItem("token")
+        window.location.reload()
+    }
+
     return (
+        <div className="flex min-h-screen bg-gray-100 dark:bg-black">
+            <div className="m-auto">
+                <h1 className="dark:text-gray-200">Boats</h1>
+                <button onClick={logout} className="w-40 py-1 rounded-lg bg-red-600 hover:ring ring-red-800 text-white transition duration-200">Logout</button>
+            </div>
+        </div>
     );
 }
 
