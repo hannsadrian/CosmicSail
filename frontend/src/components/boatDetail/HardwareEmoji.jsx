5b232201f3dc7c4cd1b4b65cdf55b8b2b079f0ae
Adrian <hannsadrian@gmail.com>
2021-01-12 21:36:03 +0100

    Update HardwareEmoji component

---
+++ frontend/src/components/boatDetail/HardwareEmoji.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const hardwares = [
+    {name: "gps", emoji: "🌍"},
+    {name: "gps_error", emoji: "🚧"},
+    {name: "bandwidth", emoji: "📟"},
+    {name: "ip", emoji: "🏷"},
+    {name: "bno", emoji: "🧭"},
+    {name: "locating", emoji: "🔎"},
+    {name: "wind", emoji: "🍃"},
+    {name: "autopilot", emoji: "🤖"},
+    {name: "shore", emoji: "🏝"},
+    {name: "rudder", emoji: "🛶"},
+    {name: "sail", emoji: "⛵"},
+    {name: "engine", emoji: "⛽️"},
+]
+
+const HardwareEmoji = ({hardware}) => {
+    let emoji = hardwares.find(h => h.name === hardware)
+
+    return <>{emoji && emoji.emoji}</>
+};
+
+export default HardwareEmoji;
