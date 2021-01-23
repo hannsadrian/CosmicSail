f7880e586b719bd8c2e200f9240247023f702937
Adrian <hannsadrian@gmail.com>
2021-01-23 02:48:19 +0100

    Print resulting forward force! :tada:

--- boat/simulation/simulation.py
+++ boat/simulation/simulation.py
@@ -21,12 +21,13 @@ class Simulation:
 
     def update(self) -> None:
         sail_area = 0.366  # in m**2
-        wind_speed = 1  # in m/s
-        wind_force = (sail_area ** 2) * (1.229 ** 3) * wind_speed ** 2  # results in Newton
-        wind_direction = 10  # degrees
-        heading = 300  # degrees
+        air_density = 1.229  # in kg/m**3
+        wind_speed = 3  # in m/s
+        wind_force = (sail_area ** 2) * (air_density ** 3) * wind_speed ** 2  # results in Newton
+        wind_direction = 90  # degrees
+        heading = 0  # degrees
 
-        sail = 1  # sail value from 0 to -1
+        sail = 0.81  # sail value from -1 to 1
         beta = 2 * math.asin(
             math.sqrt((6 + (sail + 1) * 3) ** 2 - 36) / 21)  # degrees of elongation from middle of boat
         # find smallest possible angle from wind_dir to heading
@@ -34,13 +35,15 @@ class Simulation:
             wind_direction - heading))
         alpha = gamma - beta
 
-        wind_vector = vector_from_heading((wind_direction + 180) % 360)
-        wind_vector = wind_vector * wind_force
+        f1 = vector_from_heading(math.degrees(gamma))  # wind vector
+        f1 = f1 * wind_force
 
-        f3 = vector_from_heading((beta + math.radians(90)) % 360)
+        f3 = vector_from_heading(math.degrees(beta + math.radians(90)) % 360)
 
-        f3 *= alpha * math.radians(90)
-        print(math.degrees(math.atan2(f3.y, f3.x)))
-        print(abs(f3))
+        print(math.degrees(alpha))
+
+        f2 = f1 * (1 - alpha * 2 / math.pi)
+        f3 = f1 * alpha * 2 / math.pi
+        print(f3.y) # this is the forward force on the boat!
 
         return None
