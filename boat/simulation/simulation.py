fb09ba9e5112dc0b1410a0e58c361d905497ac3d
Adrian <hannsadrian@gmail.com>
2021-01-23 15:19:05 +0100

    Clean up the forward force calculation :sparkles:

--- boat/simulation/simulation.py
+++ boat/simulation/simulation.py
@@ -20,9 +20,9 @@ class Simulation:
         return None
 
     def update(self) -> None:
-        sail = 0.81  # sail value from -1 to 1
+        sail = 1  # sail value from -1 to 1
         wind_speed = 3  # in m/s
-        wind_direction = 90  # degrees
+        wind_direction = 270  # degrees
         heading = 0  # degrees
 
         sail_area = 0.366  # in m**2
@@ -30,23 +30,22 @@ class Simulation:
 
         wind_force = (sail_area ** 2) * (air_density ** 3) * wind_speed ** 2  # results in Newton
 
-        beta = 2 * math.asin(
-            math.sqrt((6 + (sail + 1) * 3) ** 2 - 36) / 21)  # degrees of elongation from middle of boat
-
         # find smallest possible angle from wind_dir to heading
         gamma = math.radians(360 - abs(wind_direction - heading) if abs(wind_direction - heading) > 180 else abs(
             wind_direction - heading))
-        
-        alpha = gamma - beta
+
+        beta = 2 * math.asin(math.sqrt((6 + (sail + 1) * 3) ** 2 - 36) / 21)  # elongation from middle of boat
+        if beta > gamma:
+            beta = gamma
+
+        # alpha = gamma - beta
+        alpha = math.radians(180) - abs(gamma - beta) if abs(gamma - beta) > math.radians(90) else abs(gamma - beta)
 
         f1 = vector_from_heading(math.degrees(gamma))  # wind vector
         f1 = f1 * wind_force
 
-        # TODO: could be removed?
-        f3 = vector_from_heading(math.degrees(beta + math.radians(90)) % 360)
-
         f2 = f1 * (1 - alpha * 2 / math.pi)  # force that's "reflected" by the sails
-        f3 = f1 * alpha * 2 / math.pi  # actual force on the sails
-        print(f3.y)  # this is the forward force on the boat!
+        f3 = vector_from_heading(math.degrees(beta) + 90) * abs(f1 * alpha * 2 / math.pi)  # actual force on the sails
+        print("forward force:", abs(f3.x))  # this is the forward force on the boat!
 
         return None
