a7b0b66ec90742c455e63e1927e68e588852f33f
Adrian <hannsadrian@gmail.com>
2021-01-23 03:20:48 +0100

    Refactor structure

--- boat/simulation/simulation.py
+++ boat/simulation/simulation.py
@@ -20,30 +20,33 @@ class Simulation:
         return None
 
     def update(self) -> None:
-        sail_area = 0.366  # in m**2
-        air_density = 1.229  # in kg/m**3
+        sail = 0.81  # sail value from -1 to 1
         wind_speed = 3  # in m/s
-        wind_force = (sail_area ** 2) * (air_density ** 3) * wind_speed ** 2  # results in Newton
         wind_direction = 90  # degrees
         heading = 0  # degrees
 
-        sail = 0.81  # sail value from -1 to 1
+        sail_area = 0.366  # in m**2
+        air_density = 1.229  # in kg/m**3
+
+        wind_force = (sail_area ** 2) * (air_density ** 3) * wind_speed ** 2  # results in Newton
+
         beta = 2 * math.asin(
             math.sqrt((6 + (sail + 1) * 3) ** 2 - 36) / 21)  # degrees of elongation from middle of boat
+
         # find smallest possible angle from wind_dir to heading
         gamma = math.radians(360 - abs(wind_direction - heading) if abs(wind_direction - heading) > 180 else abs(
             wind_direction - heading))
+        
         alpha = gamma - beta
 
         f1 = vector_from_heading(math.degrees(gamma))  # wind vector
         f1 = f1 * wind_force
 
+        # TODO: could be removed?
         f3 = vector_from_heading(math.degrees(beta + math.radians(90)) % 360)
 
-        print(math.degrees(alpha))
-
-        f2 = f1 * (1 - alpha * 2 / math.pi)
-        f3 = f1 * alpha * 2 / math.pi
-        print(f3.y) # this is the forward force on the boat!
+        f2 = f1 * (1 - alpha * 2 / math.pi)  # force that's "reflected" by the sails
+        f3 = f1 * alpha * 2 / math.pi  # actual force on the sails
+        print(f3.y)  # this is the forward force on the boat!
 
         return None
