6b48cb4d71627b319696176a7d6bc56cb9750ab6
Adrian <hannsadrian@gmail.com>
2021-03-10 21:52:28 +0100

    Update simulation formular

--- boat/simulation/boat.py
+++ boat/simulation/boat.py
@@ -17,11 +17,14 @@ def get_forward_force_by_wind(sail, wind_speed, wind_direction, heading) -> floa
     gamma = math.radians(360 - abs(wind_direction - heading) if abs(wind_direction - heading) > 180 else abs(
         wind_direction - heading))
 
+    min_sheet_len = 6
+    max_sheet_len = 12
+    boom_len = 21
     # 2 * asin(sqrt(c^2-b^2)/21)
     # calculate elongation from middle of boat with minimal offset
     # -> the sail is never straight over the middle of the boat, as this calculation proposes without offset
-    # latex: \beta = 2\arcsin{\frac{\sqrt{(\lambda + (\eta + 1) * \frac{\lambda}{2})^2 - \lambda^2}}{\tau}} + 0.17453
-    beta = 2 * math.asin(math.sqrt((6 + (sail + 1) * 3) ** 2 - 36) / 21) + 0.17453
+    # latex: \beta = 2\arcsin{\frac{\sqrt{(a + \frac{(x + 1)}{2} * (q-a))^2 - a^2}}{2b}} + 0.17453
+    beta = 2 * math.asin(math.sqrt((min_sheet_len + (sail + 1)/2 * (max_sheet_len - min_sheet_len)) ** 2 - min_sheet_len ** 2) / 2*boom_len) + 0.17453
     # if the sail elongation is greater than the wind, simulate sail flutter in the wind
     if beta > gamma:
         beta = gamma
